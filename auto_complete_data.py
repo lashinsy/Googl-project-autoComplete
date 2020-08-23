e78bd243d684a1ccb0eee52505949004560b4302
lashinsy <lch@sem-wolf.org>
2020-08-23 03:41:51 +0300

    add class AutoCompleteData

---
+++ auto_complete_data.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class AutoCompleteData:
+
+    def __init__(self, sentence, source_text, offset, score):
+        self.completed_sentence = sentence
+        self.source_text = source_text
+        self.offset = offset
+        self.score = score
