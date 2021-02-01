f66b05bb7fac4e807d0040d951701c33c309ef52
lashinsy <chana.leshin@gmail.com>
2021-02-01 17:22:53 +0200

    add default value

--- auto_complete_data.py
+++ auto_complete_data.py
@@ -1,10 +1,6 @@
-from dataclasses import dataclass
-
-
-@dataclass
 class AutoCompleteData:
 
-    def __init__(self, sentence, source_text, offset, score):
+    def __init__(self, sentence, source_text, offset, score=0):
         self.completed_sentence = sentence
         self.source_text = source_text
         self.offset = offset
